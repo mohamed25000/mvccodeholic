3399e74b64dc4f14ba83ef155d590c7b01aa1797
Mohamed Mennour <mohamedmennour25@gmail.com>
2022-12-08 10:16:15 +0100

    first commit

---
+++ views/register.php
@@ -0,0 +1,34 @@
+<h1>Create account</h1>
+<form action="" method="post">
+    <div class="row">
+        <div class="col">
+            <div class="mb-3">
+                <label class="form-label">First name</label>
+                <input type="text" name="firstname" class="form-control">
+                <div class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="mb-3">
+                <label class="form-label">Last name</label>
+                <input type="text" name="lastname" class="form-control">
+                <div class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+        </div>
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Email</label>
+        <input type="email" name="email" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Password</label>
+        <input type="password" name="password" class="form-control">
+        <div class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Confirm Password</label>
+        <input type="password" name="confirmpassword" class="form-control">
+        <div class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
